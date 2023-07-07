d7c3c47d2bda105106c54200aad4070bb88d13c4
Arves777 <adrianrosselis2017@gmail.com>
2023-07-07 14:08:50 -0300

    Seller entity class

--- src/application/Progam.java
+++ src/application/Progam.java
@@ -1,10 +1,13 @@
 package application;
 
+import java.util.Date;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Progam {
     public static void main(String[] args){
-        Department obj = new Department(1,"Games");
-        System.out.println(obj);
+        Department department = new Department(1,"Games");
+        Seller seller = new Seller(7, "Rodrigo", "rodrigo@outlook.com", new Date(), 3000.0, department);
+        System.out.println(seller);
     }
 }
